c95c693bd1a8e382c5d2260e488212b7d1cfd30d
Apinan Woratrakun <iamapinan@Apinans-Air-M2.local>
2024-03-07 02:31:46 +0700

    fix pwa

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = function override(config, env) {
     const workboxConfig = {
         ...defaultInjectConfig,
-        swSrc: path.join(__dirname, 'src', 'service-worker.js'),
+        swSrc: path.join(__dirname, 'public', 'service-worker.js'),
         swDest: 'service-worker.js',
     };
     config.plugins.push(new InjectManifest(workboxConfig));
