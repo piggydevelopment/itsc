9456037271c3a6c81383277610e5b95aea383e79
Apinan Woratrakun <iamapinan@gmail.com>
2024-04-22 11:43:31 +0700

    update orgid

--- src/configs/app.jsx
+++ src/configs/app.jsx
@@ -1,10 +1,11 @@
 
-export const mockUrl = 'https://synzegat.wiremockapi.cloud';
+// export const mockUrl = 'https://synzegat.wiremockapi.cloud';
 export const apiUrl = 'https://corp-api.synzofficial.com';
 export const apiToken = '';
 export const cs_token = 'yoJJA4KDq7gBdYHN9AUtL6iU';
 export const cs_identifier = 'bwZRXVXPKN73M3b7AdtBobcM';
 export const cs_hmac_secret = 'jb1mtajroy64cp3wjaxnagsxs2lr4j8t';
+export const orgID = 2;
 // export const cs_token = 'dvnz1PDNigwetK24P9WoeMqF';
 export const cs_url = 'https://cs.synzofficial.com';
 export const areas = [
